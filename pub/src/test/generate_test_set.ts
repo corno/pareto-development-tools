411d53c2e94e562baa4cb07195e6606eb3691061
Corno Schraverus <corno@schraverus.com>
2025-10-30 04:21:41 +0100

    .

---
+++ pub/src/test/generate_test_set.ts
@@ -0,0 +1,14 @@
+import { Package_Cluster_State } from "../interface/package_state"
+
+import { $$ as analyse_cluster } from "../queries/analyse_cluster"
+
+import * as fs from "fs"
+
+
+export const $$ = (path_to_cluster: string, test_name: string): void => {
+
+    const result: Package_Cluster_State = analyse_cluster(path_to_cluster)
+
+   fs.writeFileSync(`../tests/analysed_structures/${test_name}.json`, JSON.stringify(result, null, 4))
+
+}
