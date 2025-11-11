43afc55e0941f26abc9c8d25d9a4f1873d43e7f7
Corno Schraverus <corno@schraverus.com>
2025-11-11 21:00:44 +0100

    all resources are now injected

--- pub/src/bin.ts
+++ pub/src/bin.ts
@@ -2,79 +2,77 @@
 
 import * as _eb from 'exupery-core-bin'
 
+import * as d_bin from "./implementation/algorithms/procedures/unguaranteed/bin"
+
 import { $$ as p_bin } from "./implementation/algorithms/procedures/unguaranteed/bin"
 
-import { $$ as q_eaqe } from "exupery-resources/dist/implementation/algorithms/queries/unguaranteed/execute_any_query_executable"
-import { $$ as q_read_directory } from "exupery-resources/dist/implementation/algorithms/queries/unguaranteed/read_directory"
-
-import { $$ as p_eape } from "exupery-resources/dist/implementation/algorithms/procedures/unguaranteed/execute_any_procedure_executable"
-import { $$ as p_easpe } from "exupery-resources/dist/implementation/algorithms/procedures/unguaranteed/execute_any_smelly_procedure_executable"
-import { $$ as p_log } from "exupery-resources/dist/implementation/algorithms/procedures/guaranteed/log"
-import { $$ as p_write_to_stderr } from "exupery-resources/dist/implementation/algorithms/procedures/guaranteed/write_to_stderr"
-
-
-_eb.run_unguaranteed_main_procedure(($p, $r) => {
-    return p_bin($p, {
-        'queries': {
-            'git': ($p, $r) => {
-                return q_eaqe(
-                    {
-                        'program': `git`,
-                        'args': $p.args,
-                    },
-                    null
-                )
+_eb.run_unguaranteed_main_procedure<d_bin.Resources>(
+    ($rr) => {
+        return {
+            'queries': {
+                'git': ($p, $r) => {
+                    return $rr.queries['execute any query executable'](
+                        {
+                            'program': `git`,
+                            'args': $p.args,
+                        },
+                        null
+                    )
+                },
+                'read directory': ($p, $r) => {
+                    return $rr.queries['read directory']($p, null)
+                },
             },
-            'read directory': ($p, $r) => {
-                return q_read_directory($p, null)
+            'procedures': {
+                'git': ($p, $r) => {
+                    return $rr.procedures['execute any procedure executable'](
+                        {
+                            'program': `git`,
+                            'args': $p.args,
+                        },
+                        null
+                    )
+                },
+                'npm': ($p, $r) => {
+                    return $rr.procedures['execute any procedure executable'](
+                        {
+                            'program': `npm`,
+                            'args': $p.args,
+                        },
+                        null
+                    )
+                },
+                'tsc': ($p, $r) => {
+                    return $rr.procedures['execute any smelly procedure executable'](
+                        {
+                            'program': `tsc`,
+                            'args': $p.args,
+                        },
+                        null
+                    )
+                },
+                'update2latest': ($p, $r) => {
+                    return $rr.procedures['execute any procedure executable'](
+                        {
+                            'program': `update2latest`,
+                            'args': $p.args,
+                        },
+                        null
+                    )
+                },
+                'log': ($p, $r) => {
+                    return $rr.procedures.log(
+                        $p,
+                        null
+                    )
+                },
+                'write to stderr': ($p, $r) => {
+                    return $rr.procedures['write to stderr']($p, null)
+                },
             },
-        },
-        'procedures': {
-            'git': ($p, $r) => {
-                return p_eape(
-                    {
-                        'program': `git`,
-                        'args': $p.args,
-                    },
-                    null
-                )
-            },
-            'npm': ($p, $r) => {
-                return p_eape(
-                    {
-                        'program': `npm`,
-                        'args': $p.args,
-                    },
-                    null
-                )
-            },
-            'tsc': ($p, $r) => {
-                return p_easpe(
-                    {
-                        'program': `tsc`,
-                        'args': $p.args,
-                    },
-                    null
-                )
-            },
-            'update2latest': ($p, $r) => {
-                return p_eape(
-                    {
-                        'program': `update2latest`,
-                        'args': $p.args,
-                    },
-                    null
-                )
-            },
-            'log': ($p, $r) => {
-                return p_log(
-                    $p,
-                    null
-                )
-            },
-            'write to stderr': ($p, $r) => {
-                return p_write_to_stderr($p, null)
-            },
-        },
-    })
-})
+        }
+    },
+    ($p, $r) => {
+        return p_bin($p, $r)
+    }
+)
