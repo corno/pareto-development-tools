87db9711766eff5f693058a74e30a75cba6e410c
Corno Schraverus <corno@schraverus.com>
2026-01-08 00:01:45 +0100

    ...

--- pub/src/modules/npm/implementation/manual/commands/npm_publish.ts
+++ pub/src/modules/npm/implementation/manual/commands/npm_publish.ts
@@ -1,7 +1,6 @@
 import * as _p from 'pareto-core-command'
 import * as _pt from 'pareto-core-transformer'
 import * as _pi from 'pareto-core-interface'
-import * as _pdev from 'pareto-core-dev'
 
 import * as signatures from "../../../interface/signatures"
 
@@ -18,7 +17,7 @@ export const $$: signatures.commands.npm_publish = _p.command_procedure(
                     ],
                     $p.path.transform(
                         ($) => _pt.list.literal([
-                            _pdev.log_debug_message(s_path.Context_Path($), () => s_path.Context_Path($)),
+                             s_path.Context_Path($),
                         ]),
                         () => _pt.list.literal([])
                     ),
