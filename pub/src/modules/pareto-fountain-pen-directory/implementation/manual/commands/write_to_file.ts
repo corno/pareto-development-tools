ae23871c263128ad2232e7835ceb57873b14ad35
Corno Schraverus <corno@schraverus.com>
2026-01-18 11:19:37 +0100

    ...

--- pub/src/modules/pareto-fountain-pen-directory/implementation/manual/commands/write_to_file.ts
+++ pub/src/modules/pareto-fountain-pen-directory/implementation/manual/commands/write_to_file.ts
@@ -2,6 +2,7 @@
 import * as _pt from 'pareto-core-transformer'
 import * as _pi from 'pareto-core-interface'
 import * as _p from 'pareto-core-command'
+import * as _p_ser from 'pareto-core-serializer'
 
 import * as signatures from "../../../interface/signatures"
 
@@ -10,9 +11,14 @@ import * as d_write_to_file from "../../../interface/to_be_generated/write_to_fi
 
 //dependencies
 import * as t_block_2_lines from "pareto-fountain-pen/dist/implementation/manual/schemas/block/transformers/lines"
-import { $$ as s_list_of_texts } from "pareto-standard-operations/dist/implementation/temp_serializers/schemas/list_of_texts"
 import * as t_path_to_path from "pareto-resources/dist/implementation/manual/schemas/path/transformers/path"
 import { replace_space_in_context_path } from "../schemas/path/transformers/path"
+
+const s_list_of_texts: _pi.Serializer<_pi.List<string>> = ($) => _p_ser.text.deprecated_build(($i) => {
+    $.__for_each(($) => {
+        $i['add snippet']($)
+    })
+})
 
 export const $$: signatures.commands.write_to_file = _p.command_procedure(
     ($p, $cr) => [
