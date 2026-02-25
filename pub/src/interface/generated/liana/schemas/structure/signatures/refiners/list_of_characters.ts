dda3530539993f282bb192a4d83157cc056c4661
Corno Schraverus <corno@schraverus.com>
2026-02-25 01:15:30 +0100

    pdt: ...

--- pub/src/interface/generated/liana/schemas/structure/signatures/refiners/list_of_characters.ts
+++ pub/src/interface/generated/liana/schemas/structure/signatures/refiners/list_of_characters.ts
@@ -5,7 +5,7 @@ import * as i_generic from "liana-core/dist/interface/to_be_generated/deserializ
 
 import * as i_in from "pareto-fountain-pen/dist/interface/generated/liana/schemas/list_of_characters/data"
 
-import * as i_out from "./data"
+import * as i_out from "../../data"
 
 export namespace Directory_ {
     
--- pub/src/interface/generated/liana/schemas/structure/signatures/transformers/fountain_pen.ts
+++ pub/src/interface/generated/liana/schemas/structure/signatures/transformers/fountain_pen.ts
@@ -1,15 +1,15 @@
 
 import * as _pi from 'pareto-core/dist/interface'
 
-import * as i_out from "astn-core/dist/interface/generated/liana/schemas/sealed_target/data"
+import * as i_in from "../../data"
 
-import * as i_in from "./data"
+import * as i_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
 
 export namespace Directory_ {
     
     export type I = i_in.Directory
     
-    export type O = i_out.Value
+    export type O = i_out.Paragraph
     
     export namespace P {
         
