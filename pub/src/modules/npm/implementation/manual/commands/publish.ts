fc5faeb40bf8237cecb87f27b4f9e510881ec38f
Corno Schraverus <corno@schraverus.com>
2026-03-07 09:16:38 +0100

    pdt: ...

--- pub/src/modules/npm/implementation/manual/commands/publish.ts
+++ pub/src/modules/npm/implementation/manual/commands/publish.ts
@@ -11,20 +11,11 @@ export const $$: signatures.commands.npm_publish = _p.command_procedure(
     ($p, $cr) => [
         $cr['npm'].execute(
             {
-                'working directory': _p.optional.literal.not_set(),
+                'working directory': $p.path,
                 'args': _pt.list.nested_literal_old([
                     [
                         "publish"
                     ],
-                    //publish doesn't support --prefix, so we have to use a path to the package as an argument instead
-                    //but this has issues as well. If 
-
-                    $p.path.__decide(
-                        ($) => _pt.list.literal([
-                            t_path_to_text.Context_Path($),
-                        ]),
-                        () => _pt.list.literal([])
-                    ),
                     _p.decide.state($p.impact, ($) => {
                         switch ($[0]) {
                             case 'dry run': return _p.ss($, ($) => ["--dry-run"])
