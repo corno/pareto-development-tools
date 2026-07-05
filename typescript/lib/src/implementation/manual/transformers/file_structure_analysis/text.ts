50dc271d0dc2e7d8403cd848f296fd0ab1a7c5d2
Corno Schraverus <corno@schraverus.com>
2026-07-05 09:53:18 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/transformers/file_structure_analysis/text.ts
+++ typescript/lib/src/implementation/manual/transformers/file_structure_analysis/text.ts
@@ -5,10 +5,17 @@ import p_list_from_text from 'pareto-core/dist/implementation/refiner/specials/l
 
 //data types
 import * as d_in from "../../../../interface/data/file_structure_analysis"
-export const Path: p_i.Transformer<
-    d_in.Path,
-    string
-> = ($) => p_text_from_list(
+import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/text/data"
+
+export namespace interface_ {
+    export type Path = p_i.Transformer<
+        d_in.Path,
+        d_out.Text
+    >
+}
+
+
+export const Path: interface_.Path = ($) => p_text_from_list(
     p_.from.list($).flatten(
         ($) => p_.literal.segmented_list([
             p_.literal.list([
