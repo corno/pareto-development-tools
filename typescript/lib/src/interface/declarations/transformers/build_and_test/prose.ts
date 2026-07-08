85d8cb3c1f8eb4e41fdfe5bb7316fe7ef9919dc8
Corno Schraverus <corno@schraverus.com>
2026-07-08 21:30:09 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/build_and_test/prose.ts
+++ typescript/lib/src/interface/declarations/transformers/build_and_test/prose.ts
@@ -5,15 +5,6 @@ import type * as p_ from 'pareto-core/interface/transformer'
 import type * as d_in from "../../../data/build_and_test.js"
 import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
 
-//shorthands
-import * as sh from "pareto-fountain-pen/shorthands/prose/deprecated"
-
-//dependencies
-import * as t_build_to_prose from "../build/prose.js"
-import * as t_epe_to_prose from "pareto-resources/implementation/manual/transformers/execute_command_executable/prose"
-
-
-
 export type Error = p_.Transformer_With_Parameter<
     d_in.Error,
     d_out.Phrase,
--- typescript/lib/src/interface/declarations/transformers/file_structure_analysis/csv.ts
+++ typescript/lib/src/interface/declarations/transformers/file_structure_analysis/csv.ts
@@ -5,11 +5,6 @@ import type * as p_ from 'pareto-core/interface/transformer'
 import type * as d_in from "../../../data/file_structure_analysis.js"
 import type * as d_out from "../../../../modules/csv/interface/data/csv.js"
 
-//dependencies
-import * as t_to_text from "./text.js"
-
-
-
 export type Signature = p_.Transformer<
     d_in.File_Analysis_List,
     d_out.CSV
